ee78d2488c50f09e5e354c0554978b18fd462abc
hiecxz123 <2277520399@qq.com>
2022-07-25 01:08:09 +0800

    添加单例脚本 Signed-off-by: hiecxz123 <2277520399@qq.com>

---
+++ Assets/CFramework/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton <T>where T:new()
+{
+    private static T instance;
+    public static T GetInstance()
+    {
+        if(instance==null)
+        {
+            instance = new T();
+        }
+        return instance;
+    }
+}
---
+++ Assets/CFramework/Scripts/Singleton/SingletonAutoMono.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//调用GetInstance()会创建一个空物体并挂载相应脚本
+//切换场景会被删除
+public class SingletonAutoMono<T> : MonoBehaviour where T:MonoBehaviour
+{
+    private static T instance;
+    public static T GetInstance()
+    {
+        if(instance==null)
+        {
+            GameObject obj = new GameObject();
+            obj.name = typeof(T).ToString();
+            //让单例模式对象过场景不移除
+
+            DontDestroyOnLoad(obj);
+
+            instance = obj.AddComponent<T>();
+        }
+        return instance;
+    }
+    
+}
